4e1c2b4ac6475cc1b393dcd72899f61961a6a911
badlogic <badlogic@gmail.com>
2024-12-13 10:47:28 +0100

    Fix up torch dependencies, CUDA on Windows/Linux assumed

--- src/__init__.py
+++ src/__init__.py
@@ -8,9 +8,6 @@ def get_device():
     elif torch.backends.mps.is_available():
         device = torch.device("mps")
         print("Using MPS")
-    elif torch.backends.opencl.is_available():
-        device = torch.device("opencl")
-        print("Using OpenCL")
     else:
         device = torch.device("cpu")
         print("Using CPU")
